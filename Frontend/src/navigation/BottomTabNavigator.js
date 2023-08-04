4bd3a829d805ae4401796d1fbb527442eb43b016
VayneXPP <ziwanm@student.unimelb.edu.au>
2023-08-04 19:05:37 +1000

    create BottomTabNavigator and add icon

--- Frontend/src/navigation/BottomTabNavigator.js
+++ Frontend/src/navigation/BottomTabNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 import BrowsePostsScreen from '../screens/BrowsePostsScreen';
 import ShoppingCartScreen from '../screens/ShoppingCartScreen';
 import CreatePostScreen from '../screens/CreatePostScreen';
@@ -10,9 +11,60 @@ const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
   return (
-    <Tab.Navigator initialRouteName="Browse">
-      <Tab.Screen name="Browse" component={BrowsePostsScreen} />
-      <Tab.Screen name="Shopping" component={ShoppingCartScreen} />
+    <Tab.Navigator
+      initialRouteName="Browse"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let icon;
+
+          if (route.name === 'Browse') {
+            icon = focused
+              ? require('../../../assets/icons/act_home.png')
+              : require('../../../assets/icons/inact_home.png');
+          } else if (route.name === 'Shopping') {
+            icon = focused
+              ? require('../../../assets/icons/act_cart.png')
+              : require('../../../assets/icons/inact_cart.png');
+          }else if (route.name === 'CreatePost') {
+            icon = focused
+              ? require('../../../assets/icons/add.png')
+              : require('../../../assets/icons/add.png');
+          }else if (route.name === 'Messages') {
+            icon = focused
+              ? require('../../../assets/icons/act_chat.png')
+              : require('../../../assets/icons/inact_chat.png');
+          }else if (route.name === 'Profile') {
+            icon = focused
+              ? require('../../../assets/icons/act_user.png')
+              : require('../../../assets/icons/inact_user.png');
+          }
+
+
+          return <Image source={icon} style={{ width: 20, height: 20 }} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen
+        name="Browse"
+        component={BrowsePostsScreen}
+        options={({ focused }) => ({
+          tabBarLabelStyle: {
+            fontWeight: focused ? 'bold' : 'normal',
+          },
+        })}
+      />
+      <Tab.Screen
+        name="Shopping"
+        component={ShoppingCartScreen}
+        options={({ focused }) => ({
+          tabBarLabelStyle: {
+            fontWeight: focused ? 'bold' : 'normal',
+          },
+        })}
+      />
+      {/* 为其他屏幕重复上述设置 */}
       <Tab.Screen name="CreatePost" component={CreatePostScreen} />
       <Tab.Screen name="Messages" component={MessagesScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
